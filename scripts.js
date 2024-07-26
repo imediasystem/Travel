4af21a0618c1b0fb653838704f92b4d37ed051cd
iMedia SysteM <imediasystem@hotmail.com>
2024-07-26 20:49:21 +0200

    Responsive navigation menu

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var collapsible = document.querySelectorAll(".collapsible");
+collapsible.forEach((item) =>
+{
+    item.addEventListener("click", function (){
+        this.classList.toggle("collapsible--expanded")
+    })
+})
